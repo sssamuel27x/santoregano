55400cf06e3e94dfab854a65a75256752052f3ee
Samuel Ramos <samuelmau2008@gmail.com>
2026-08-16 01:01:33 +0100

    Fix route navigation crash

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ import { useRestaurantStatus } from './hooks/useRestaurantStatus';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-  useEffect(() => window.scrollTo({ top: 0, behavior: 'instant' }), [pathname]);
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' });
+  }, [pathname]);
   return null;
 }
 
